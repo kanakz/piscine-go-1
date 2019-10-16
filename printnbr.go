bb259d6b2564e3f5d011d53a8c41be33ad0d9406
MirasK <mirkrokok0@gmail.com>
2019-10-16 21:19:52 +0600

    q2e7

--- printnbr.go
+++ printnbr.go
@@ -15,13 +15,13 @@ func SetNbr(n int) {
 		z01.PrintRune(a)
 		return
 	}
-	for i := 1; i <= n % 10; i++ {
+	for i := 1; i <= n%10; i++ {
 		a++
 	}
-	for i := -1; i >= n % 10; i-- {
+	for i := -1; i >= n%10; i-- {
 		a++
 	}
-	if n / 10 != 0 {
+	if n/10 != 0 {
 		SetNbr(n / 10)
 	}
 	z01.PrintRune(a)
