69be069c57437ba9e47ec8a21be25d9b834e7b21
MirasK <mirkrokok0@gmail.com>
2019-10-16 19:50:48 +0600

    q2e7

--- printnbr.go
+++ printnbr.go
@@ -2,6 +2,13 @@ package piscine
 
 import "github.com/01-edu/z01"
 
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+	}
+	SetNbr(n)
+}
+
 func SetNbr(n int) {
 	a := '0'
 	if n == 0 {
@@ -19,11 +26,4 @@ func SetNbr(n int) {
 	}
 	z01.PrintRune(a)
 	return
-}
-
-func PrintNbr(n int) {
-	if n < 0 {
-		z01.PrintRune('-')
-	}
-	SetNbr(n)
 }
