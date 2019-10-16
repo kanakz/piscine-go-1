800d2772e850a4b93cf9bda5806184d565752db7
MirasK <mirkrokok0@gmail.com>
2019-10-16 11:50:24 +0600

    2qe5

---
+++ printcomb.go
@@ -0,0 +1,30 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := '0'; i <= '7'; i++ {
+		for j := '1'; j <= '8'; j++ {
+			for k := '2'; k <= '9'; k++ {
+				if i < j && j < k {
+					if i == '7' && j == '8' && k == '9' {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(k)
+					} else {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(k)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	PrintComb()
+}
