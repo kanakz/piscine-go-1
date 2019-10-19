7bb4908274b77911772b720914c25e2ed2e9f8b4
MirasK <mirkrokok0@gmail.com>
2019-10-19 17:02:26 +0600

    r1a

--- raid1a.go
+++ raid1a.go
@@ -3,29 +3,26 @@ package student
 import "github.com/01-edu/z01"
 
 func Raid1a(x, y int) {
-	if x <= 0 && y <= 0 {
-		z01.PrintRune('\n')
-	}
-
-	for j := 1; j <= y; j++ {
-		if j == y || j == 1 {
-			for i := 1; i <= x; i++ {
-				if i == 1 || i == x {
-					z01.PrintRune('o')
-				} else {
-					z01.PrintRune('-')
+	if x > 0 && y > 0 {
+		for j := 1; j <= y; j++ {
+			if j == y || j == 1 {
+				for i := 1; i <= x; i++ {
+					if i == 1 || i == x {
+						z01.PrintRune('o')
+					} else {
+						z01.PrintRune('-')
+					}
+				}
+			} else {
+				for i := 1; i <= x; i++ {
+					if i == 1 || i == x {
+						z01.PrintRune('|')
+					} else {
+						z01.PrintRune(' ')
+					}
 				}
 			}
-		} else {
-			for i := 1; i <= x; i++ {
-				if i == 1 || i == x {
-					z01.PrintRune('|')
-				} else {
-					z01.PrintRune(' ')
-				}
-			}
+			z01.PrintRune('\n')
 		}
-		z01.PrintRune('\n')
 	}
-
 }
