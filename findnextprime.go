972621ba7be9db4c83a43fcccd814269668cc590
MirasK <mirkrokok0@gmail.com>
2019-10-21 13:45:20 +0600

    q4e8

--- findnextprime.go
+++ findnextprime.go
@@ -1,34 +1,49 @@
 package piscine
 
-func Prime(nb int) bool {
-	k := 0
-	for i := 1; i <= nb; i++ {
-		if nb%i == 0 {
-			k++
+func Prime(x int) bool {
+	if x <= 1 {
+		return false
+	}
+	for i := 2; i*i <= x; i++ {
+		if x%i == 0 {
+			return false
 		}
 	}
-	if k == 2 {
-		return true
-	}
-	return false
+	return true
 }
 
 func FindNextPrime(nb int) int {
-	if nb == 1000000086 {
-		return 1000000087
-	}
-	if nb = 1000000088 {
-		return 1000000093
-	}
-	if Prime(nb) {
-		return nb
-	} else {
-		for i := nb; i <= 1000000000; i++ {
-			nb = nb + 1
-			if Prime(nb) {
-				return nb
-			}
+	for {
+		if Prime(nb) {
+			return nb
 		}
+		nb++
 	}
-	return 0
 }
+
+// func Prime(nb int) bool {
+// 	k := 0
+// 	for i := 1; i <= nb; i++ {
+// 		if nb%i == 0 {
+// 			k++
+// 		}
+// 	}
+// 	if k == 2 {
+// 		return true
+// 	}
+// 	return false
+// }
+
+// func FindNextPrime(nb int) int {
+// 	if Prime(nb) {
+// 		return nb
+// 	} else {
+// 		for i := nb; i <= 1000000000; i++ {
+// 			nb = nb + 1
+// 			if Prime(nb) {
+// 				return nb
+// 			}
+// 		}
+// 	}
+// 	return 0
+// }
