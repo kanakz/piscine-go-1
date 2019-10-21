a7ca39ac932b2b94647d98e4c7b67a974c6cb97d
MirasK <mirkrokok0@gmail.com>
2019-10-21 13:23:31 +0600

    q4e8

---
+++ findnextprime.go
@@ -0,0 +1,28 @@
+package piscine
+
+func Prime(nb int) bool {
+	k := 0
+	for i := 1; i <= nb; i++ {
+		if nb%i == 0 {
+			k++
+		}
+	}
+	if k == 2 {
+		return true
+	}
+	return false
+}
+
+func FindNextPrime(nb int) int {
+	if Prime(nb) {
+		return nb
+	} else {
+		for i := nb; i <= 1e9; i++ {
+			nb = nb + 1
+			if Prime(nb) {
+				return nb
+			}
+		}
+	}
+	return 0
+}
