0bc099db1a9c27af1b7c374efa9229e2595b2eac
MirasK <mirkrokok0@gmail.com>
2019-10-22 15:48:43 +0600

    q5e5

--- nrune.go
+++ nrune.go
@@ -2,11 +2,10 @@ package piscine
 
 func NRune(s string, n int) rune {
 	s1 := []rune(s)
-	ans := '0'
 	for index, char := range s1 {
 		if index == n-1 {
-			ans = char
+			return char
 		}
 	}
-	return ans
+	return 0
 }
